projectceladon/media-driver

commit bde3c8706e10a6d4421a2cca0f7758cfa1dfc9af

[refine] Fix CODECHAL_SETTINGS_EXT unset value usage on Linux
Values of CODECHAL_SETTINGS_EXT are not set on Linux. Replace CODECHAL_SETTINGS and CODECHAL_SETTINGS_EXT
with new class CodechalSetting and CodechalSettingExt. CodechalSettingExt will be used when Media_Reseved
set to Yes in full feature linux build.

Change-Id: I2cc21de24a48126eea5c56965c3499fef011dbe7

media_driver/linux/common/codec/ddi/media_libva_encoder.cpp
@@ -336,10 +336,8 @@ VAStatus DdiEncode_CreateContext(
 
     encCtx->pCpDdiInterface->SetHdcp2Enabled(flag);
 
-    CODECHAL_SETTINGS codecHalSettings;
-    MOS_ZeroMemory(&codecHalSettings, sizeof(codecHalSettings));
-
-    vaStatus = encCtx->m_encode->ContextInitialize(&codecHalSettings);
+    encCtx->pCpDdiInterface->SetCodechalSetting(encCtx->m_encode->m_codechalSettings);
+    vaStatus = encCtx->m_encode->ContextInitialize(encCtx->m_encode->m_codechalSettings);
 
     if (vaStatus != VA_STATUS_SUCCESS)
     {
@@ -347,7 +345,7 @@ VAStatus DdiEncode_CreateContext(
         return vaStatus;
     }
 
-    MOS_STATUS eStatus = pCodecHal->Allocate(&codecHalSettings);
+    MOS_STATUS eStatus = pCodecHal->Allocate(encCtx->m_encode->m_codechalSettings);
 
 #ifdef _MMC_SUPPORTED
     PMOS_INTERFACE osInterface = pCodecHal->GetOsInterface();

commit c3f72ef95eb73582ae79f1d11087bfef6892f93a

fix regression of Jpeg encoder caused by bde3c8706e10a6d4421a2cca0f7758cfa1dfc9af
Change-Id: I6bfba0b2c38384c343d1432c365c24faf67edeb1

media_driver/linux/common/codec/ddi/media_libva_encoder.cpp
@@ -433,6 +433,11 @@ VAStatus DdiEncode_DestroyContext(VADriverContextP ctx, VAContextID context)
     if (nullptr != encCtx->m_encode)
     {
         encCtx->m_encode->FreeCompBuffer();
+        if(nullptr != encCtx->m_encode->m_codechalSettings)
+        {
+            MOS_Delete(encCtx->m_encode->m_codechalSettings);
+            encCtx->m_encode->m_codechalSettings = nullptr;
+        }
     }
 
     MOS_FreeMemory(codecHal->GetOsInterface()->pOsContext->pPerfData);