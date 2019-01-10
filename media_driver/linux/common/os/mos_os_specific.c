57d77e651a473adaa4801c7bccaca937d1ae63d7
xuyu_(herman)__yang <herman.yang@intel.com>
2019-01-11 05:08:06 +0800

    REVERTME [IOTG]: Use CP stub for clear content to fix CTS regression
    When CP pipeline is working, some CTS tests will fail
    randomly e.g. VideoEncoderTest, DecodeAccuracyTest,
    AdaptivePlaybackTest. Using stubbed CP seems to fix this
    
    Change-Id: Ic9533e08a4fc0f1fbaa8bc48ee7b82e525229a8f
    Tracked-On: https://jira.devtools.intel.com/browse/OAM-75015
    Signed-off-by: Badiuzzaman Iskhandar <badiuzzaman.azzarfan.bin.iskhandar@intel.com>
    Reviewed-on: https://android.intel.com:443/657578

--- media_driver/linux/common/os/mos_os_specific.c
+++ media_driver/linux/common/os/mos_os_specific.c
@@ -6161,6 +6161,7 @@ MOS_STATUS Mos_Specific_InitInterface(
     }
 
     iDeviceId                                 = pOsDriverContext->iDeviceId;
+    pOsContext->RequireCPLIB                  = pOsDriverContext->RequireCPLIB;
     pOsContext->bFreeContext                  = true;
     pOsInterface->pOsContext                  = pOsContext;
     pOsInterface->bUsesPatchList              = true;
