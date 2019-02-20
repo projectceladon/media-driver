95cf92ddc5b144de08645cb2ea6ad59913504d11
Badiuzzaman Iskhandar <badiuzzaman.azzarfan.bin.iskhandar@intel.com>
2019-02-20 15:50:27 +0800

    Revert "REVERTME [IOTG]: Use CP stub for clear content to fix CTS regression"
    This reverts commit e80c0468712c118f52d44d6ec8c8fbdc111986ae.
    
    Change-Id: I1de7a0059203b00cf4bd64beaa9837ed06b07976
    Tracked-On: https://jira.devtools.intel.com/browse/OAM-76284
    Signed-off-by: Badiuzzaman Iskhandar <badiuzzaman.azzarfan.bin.iskhandar@intel.com>
    Reviewed-on: https://android.intel.com:443/661906

--- media_driver/linux/common/os/mos_os_specific.c
+++ media_driver/linux/common/os/mos_os_specific.c
@@ -6161,7 +6161,6 @@ MOS_STATUS Mos_Specific_InitInterface(
     }
 
     iDeviceId                                 = pOsDriverContext->iDeviceId;
-    pOsContext->RequireCPLIB                  = pOsDriverContext->RequireCPLIB;
     pOsContext->bFreeContext                  = true;
     pOsInterface->pOsContext                  = pOsContext;
     pOsInterface->bUsesPatchList              = true;
