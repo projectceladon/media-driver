71f1a54400c964a62fa064ae045e28a1aa68f704
Shaofeng Tang <shaofeng.tang@intel.com>
2023-02-17 11:03:21 +0800

    Upgrade to release tag: intel-media-22.6.6
    Release tag: https://github.com/intel/media-driver/releases/tag/intel-media-22.6.6
    Update Android.mk for 22.6.6
    
    Signed-off-by: Shaofeng Tang <shaofeng.tang@intel.com>

--- media_softlet/linux/common/os/osservice/mos_utilities_specific.cpp
+++ media_softlet/linux/common/os/osservice/mos_utilities_specific.cpp
@@ -40,7 +40,6 @@
 #include <sys/types.h>
 #include <sys/sem.h>
 #include <sys/mman.h>
-#include "mos_compat.h" // libc variative definitions: backtrace
 #include "mos_user_setting.h"
 #include "mos_utilities_specific.h"
 #include "mos_utilities.h"
@@ -1498,7 +1497,8 @@ MOS_STATUS MosUtilities::MosInitializeReg(RegBufferMap &regBufferMap)
     std::ifstream regStream;
     try
     {
-        regStream.open(USER_FEATURE_FILE_NEXT);
+        //regStream.open(USER_FEATURE_FILE_NEXT);
+	regStream.open("/etc/igfx_user_feature_next.txt");
         if (regStream.good())
         {
             std::string id       = "";
@@ -1575,7 +1575,8 @@ MOS_STATUS MosUtilities::MosUninitializeReg(RegBufferMap &regBufferMap)
     std::ofstream regStream;
     try
     {
-        regStream.open(USER_FEATURE_FILE_NEXT, std::ios::out | std::ios::trunc);
+        //regStream.open(USER_FEATURE_FILE_NEXT, std::ios::out | std::ios::trunc);
+	regStream.open("/etc/igfx_user_feature_next.txt", std::ios::out | std::ios::trunc);
         if (regStream.good())
         {
             for(auto pair: regBufferMap)
